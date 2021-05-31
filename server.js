b70f7e3401e8cbc0c477bd5779b6f74b66a39e19
Abhinav Chauhan <abhinavcj29@gmail.com>
2021-05-31 14:17:00 +0530

    okay

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ if (process.env.NODE_ENV === 'production') {
     });
   }
 
-
-server.listen(8080, () => console.log('server is running on port 8080'));
+var port = process.env.PORT || 8080;
+server.listen(PORT, () => console.log('server is running on port ' + PORT));
